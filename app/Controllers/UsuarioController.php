38c31e1f5763a3cc2e5f3900d36f1cec757dffb1
intinalambrico <web@internetinalambrico.com.co>
2022-04-26 14:36:24 -0500

    list usuario limit offset

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -34,7 +34,10 @@ class UsuarioController
 
     public function List(Request $request,Response $response , $id)
     {
-        $usuarios = $this->usuario->simplePaginate($id);
+        $usuarios = $this->usuario
+                ->offset($id["id"])
+                ->limit(5)
+                ->get();
         $count = $this->usuario->count();
         $responseMenssage = ["total" => $count , "usuarios" => $usuarios];
         $this->customResponse->is200Response($response, $responseMenssage);
