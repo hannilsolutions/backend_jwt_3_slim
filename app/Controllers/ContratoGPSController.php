520f16372c8e0925fadd91ce8e65c0fa89b4fcf2
intinalambrico <web@internetinalambrico.com.co>
2022-06-16 14:00:49 -0500

    prueba 5 de get gps

--- app/Controllers/ContratoGPSController.php
+++ app/Controllers/ContratoGPSController.php
@@ -115,7 +115,7 @@ class ContratoGPSController
         if($getContratosByBarrioControl != "sin registros")
         {
             $responseMessage    = $this->contratoGps
-                                    ->whereIn('id_contrato' , [$getContratosByBarrioControl]);
+                                    ->whereIn('id_contrato' , [85010, 9023, 19023]);
                                     ->get();
         }else{
             $responseMessage = "error buscando";
