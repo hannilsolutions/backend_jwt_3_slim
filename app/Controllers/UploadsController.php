b1dd577df6efea6027beb8e3528c3ee6e682207e
intinalambrico <web@internetinalambrico.com.co>
2022-06-29 16:50:35 -0500

    uploads 29062022 erro8

--- app/Controllers/UploadsController.php
+++ app/Controllers/UploadsController.php
@@ -70,12 +70,12 @@ class UploadsController
 
    public  function moveUploadedFile( $destino ,  $uploadedFile)
     {
-        //$directory = __DIR__.'/../app/Files/'.$destino;
+        $directory = __DIR__.'/../app/Files/'.$destino;
         $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
         $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
         $filename = sprintf('%s.%0.8s', $basename, $extension);
 
-        //$uploadedFile->moveTo($directory ."/". $filename);
+        $uploadedFile->moveTo($directory ."/". $filename);
 
         return $filename;
     }
