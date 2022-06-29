9fe74ae6097902828f4f57283da8ab3aca852675
intinalambrico <web@internetinalambrico.com.co>
2022-06-29 15:37:30 -0500

    uploads 29062022 erro1

--- app/Controllers/UploadsController.php
+++ app/Controllers/UploadsController.php
@@ -74,7 +74,7 @@ class UploadsController
     $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
     $filename = sprintf('%s.%0.8s', $basename, $extension);
 
-    $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+    $uploadedFile->moveTo($directory ."/". $filename);
 
     return $filename;
 }
