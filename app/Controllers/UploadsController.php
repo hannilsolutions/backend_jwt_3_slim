6354266f8db1648008ca738f3c1ca4ebabbcceb5
intinalambrico <web@internetinalambrico.com.co>
2022-06-30 15:05:17 -0500

    UPLOADS 30062022 error2

--- app/Controllers/UploadsController.php
+++ app/Controllers/UploadsController.php
@@ -56,7 +56,18 @@ class UploadsController
 
         if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
 
-            $filename = $this->moveUploadedFile(CustomRequestHandler::getParam($request,"categoria") ,$uploadedFile);
+           // $filename = $this->moveUploadedFile(CustomRequestHandler::getParam($request,"categoria") ,$uploadedFile);
+            $destino = CustomRequestHandler::getParam($request , "categoria");
+
+            $directory = __DIR__.'/Files/'.$destino;
+
+            $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+
+            $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
+        
+            $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+            $uploadedFile->moveTo($directory."/".$filename);
 
             //$response->write('uploaded ' . $filename . '<br/>');
             $responseMessage = $filename;
