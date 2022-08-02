a73c764428998d687671528c37c4ef8ef734789d
intinalambrico <web@internetinalambrico.com.co>
2022-08-02 16:53:30 -0500

    actualizacion final 3

--- app/Controllers/SGPermisoController.php
+++ app/Controllers/SGPermisoController.php
@@ -89,7 +89,7 @@ class SGPermisoController
         $indicativo = 1;
 
        // $getFindByIndicativo = $this->sgPermiso->selectRaw("indicativo from ")->get();
-       $getFindByIndicativo = $this->sgPermiso->selectRaw("indicativo")->orderBy("indicativo" , "desc")->first();
+       $getFindByIndicativo = $this->sgPermiso->selectRaw("indicativo")->where("id_empresa" , "=" , $id_empresa)->orderBy("indicativo" , "desc")->first();
 
       /*  if($getFindByIndicativo > 0)
         {
