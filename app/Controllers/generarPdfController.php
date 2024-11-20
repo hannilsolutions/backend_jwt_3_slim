83da5ac720c3783f42c5131d53b9f9404e65199b
Egue <ing.fabian23@gmail.com>
2024-11-20 15:43:52 -0500

    correcion color #12d132

--- app/Controllers/generarPdfController.php
+++ app/Controllers/generarPdfController.php
@@ -308,10 +308,10 @@ class generarPdfController
         style="font-size:10px; background:#58C3D6; color:white;">Condición Actual</td></tr>';
         foreach($aptitud as $item)
         {
-          $color = "#12d132";
-          if($item->respuest == "No")
+          $color = "#c83110";
+          if($item->respuesta == "No")
           {
-            $color = "#c83110";
+            $color = "#12d132";
           }
 
           $html .= '<tr><td>'.$item->nombre.'</td><td style="background:'.$color.'; color:white;">'.$item->respuesta.'</td></tr>';
