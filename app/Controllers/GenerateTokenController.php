76ceb1a59f9250832d9d70471f3097be47a4e55a
Egue <ing.fabian23@gmail.com>
2026-03-02 15:27:09 -0500

    git updated generate token

--- app/Controllers/GenerateTokenController.php
+++ app/Controllers/GenerateTokenController.php
@@ -5,8 +5,7 @@ namespace App\Controllers;
 
 
  
-use \Firebase\JWT\JWT;
-use \Firebase\JWT\Key;
+use \Firebase\JWT\JWT; 
 
 class GenerateTokenController  
 {
@@ -28,8 +27,8 @@ class GenerateTokenController
 
     public static function decodeToken($token)
     {
-      $secret = "d3fc3547346f0ef9cc47b9d5951912559bda2322ed3a2794d0ae49f76110dc61";
-      return JWT::decode($token, new Key($secret, "HS256"));
+      $secret = SECRET_PASSWORD;
+      return JWT::decode($token, $secret, array("HS256"));
     }
 }
 
