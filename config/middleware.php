22d66881633748b18be0636cb8b06a8e3e73fa49
Egue <ing.fabian23@gmail.com>
2026-03-03 08:57:32 -0500

    updated before

--- config/middleware.php
+++ config/middleware.php
@@ -1,28 +1,4 @@
 <?php
-return function ($app)
-{
-    $app->add(new Tuupola\Middleware\JwtAuthentication([
-        "ignore" => ["/auth/login", "/contratos/findByCus", "/contratos/gps/save", "/auth/recoveryT", "/auth/recovery", "/auth/recoveryP"],
-        "secret" => SECRET_PASSWORD,
-        "error" => function ($response, $arguments) {
-            $data["success"] = false;
-            $data["response"] = $arguments["message"];
-            $data["status_code"] = "401";
-            return $response->withHeader("Content-type", "application/json")
-                ->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-        }
-    ]));
-
-    $app->add(function ($req, $res, $next) {
-        $response = $next($req, $res);
-        return $response
-            ->withHeader("Access-Control-Allow-Origin", "*")
-            ->withHeader("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Accept,Origin,Authorization")
-            ->withHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,OPTIONS,DELETE")
-            ->withHeader("Access-Control-Allow-Credentials", "true");
-    });
-};
-/*
 #use App\Interfaces\SecretKeyInterface as Secret;
 
 
@@ -50,4 +26,4 @@ return function ($app)
            ->withHeader("Access-Control-Allow-Methods","GET,POST,PUT,PATCH,OPTIONS,DELETE")
            ->withHeader("Access-Control-Allow-Credentials","true");
     });
-};*/
+};
